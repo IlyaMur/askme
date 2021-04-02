fe6c6396d50b7390e5114c72c2f77fb037bb518d
ilyamur <ilyamur@hotmail.com>
2021-04-02 03:03:37 +0300

    initial commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,56 @@
+require 'openssl'
+
 class User < ApplicationRecord
+  ITERATIONS = 20000
+  DIGEST = OpenSSL::Digest::SHA256.new
+  REGEXP_USERNAME = /\A\w+\Z/
+
+  attr_accessor :password
+
+  has_many :questions
+
+  before_save :encrypt_password, :downcase_username
+
+  validates :email, :username, presence: true
+  validates :email, :username, uniqueness: true
+
+  validates :username, length: { maximum: 40 }, format: { with: REGEXP_USERNAME }
+  validates :email, email: {mode: :strict, require_fqdn: true}
+
+  validates :password, confirmation: true
+  validates :password, presence: true, on: :create
+
+  def self.authenticate(email, password)
+    user = find_by(email: email)
+    return nil unless user.present?
+
+    hashed_password = User.hash_to_string(
+      OpenSSL::PKCS5.pbkdf2_hmac(
+        password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST
+      )
+    )
+    return user if user.password_hash == hashed_password
+    nil
+  end
+
+  def self.hash_to_string(password_hash)
+    password_hash.unpack('H*')[0]
+  end
+
+  def encrypt_password
+    if password.present?
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+
+      self.password_hash = User.hash_to_string(
+        OpenSSL::PKCS5.pbkdf2_hmac(
+          password, password_salt, ITERATIONS, DIGEST.length, DIGEST
+        )
+      )
+    end
+  end
+
+  def downcase_username
+    username.downcase!
+  end
 end
+
