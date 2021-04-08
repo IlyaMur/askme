6fd5cc9ed75d5f67dfcf4a0a23a3aaf0f7fb2883
ilyamur <ilyamur@hotmail.com>
2021-04-08 15:56:50 +0300

    remove remove hardcode, add index and show functional

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = [
-      User.new(
-        id: 1,
-        name: 'Ilya',
-        username: 'ilyamur',
-        avatar_url: 'https://images-na.ssl-images-amazon.com/images/I/71THkrk5yOL._CR204,0,1224,1224_UX256.jpg'
-      ),
-      User.new(
-        id: 2,
-        name: 'Misha',
-        username: 'aristofun'
-      )
-    ]
+    @users = User.all
   end
 
   def new; end
@@ -20,18 +8,9 @@ class UsersController < ApplicationController
   def edit; end
 
   def show
-    @user =
-      User.new(
-        id: 1,
-        name: 'Ilya',
-        username: 'ilyamur',
-        avatar_url: 'https://images-na.ssl-images-amazon.com/images/I/71THkrk5yOL._CR204,0,1224,1224_UX256.jpg'
-      )
+    @user = User.find(params[:id])
 
-    @questions = [
-      Question.new(text: 'Сколько байт в килобайте?', created_at: Date.parse('27.03.2021')),
-      Question.new(text: 'Как дела?', created_at: Date.parse('21.05.2021'))
-    ]
+    @questions = @user.questions
 
     @questions_amount = @questions.count
     @answers_amount = @questions.select(&:answer).count
