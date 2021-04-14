0ce935640366dcfd68a644b48126d99fe9f5e23a
Ilya Mur <ilyamur@hotmail.com>
2021-04-14 21:06:36 +0300

    fix answers vulnerability

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -16,7 +16,9 @@ class QuestionsController < ApplicationController
   end
 
   def update
-    if @question.update(question_params)
+    if incorrect_answer?
+      reject_user
+    elsif @question.update(question_params)
       redirect_to user_path(@question.user), notice: 'Вопрос успешно сохранен.'
     else
       render :edit
@@ -36,13 +38,16 @@ class QuestionsController < ApplicationController
     @question = Question.find(params[:id])
   end
 
+  def incorrect_answer?
+    params[:question][:user_id].to_i != current_user.id
+  end
+
   def authorize_user
     reject_user unless current_user == @question.user
   end
 
   def question_params
-    if current_user.present? &&
-       params[:question][:user_id].to_i == current_user.id
+    if current_user.present? && params[:question][:user_id].to_i == current_user.id
       params.require(:question).permit(:user_id, :text, :answer)
     else
       params.require(:question).permit(:user_id, :text)
