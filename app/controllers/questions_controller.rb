0001988434db2e180f7b592ff5bb68cfd9106bef
Ilya Mur <ilyamur@hotmail.com>
2021-04-17 11:57:58 +0300

    refactoring question_params

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,11 +2,10 @@ class QuestionsController < ApplicationController
   before_action :load_question, only: %i[show edit update destroy]
   before_action :authorize_user, except: [:create]
 
-  def edit
-  end
+  def edit; end
 
   def create
-    @question = Question.new(question_params)
+    @question = Question.new(question_params.except(:answer))
 
     @question.author = current_user
 
@@ -43,10 +42,6 @@ class QuestionsController < ApplicationController
   end
 
   def question_params
-    if current_user.present? && params[:question][:user_id].to_i == current_user.id
-      params.require(:question).permit(:user_id, :text, :answer)
-    else
-      params.require(:question).permit(:user_id, :text)
-    end
+    params.require(:question).permit(:user_id, :text, :answer)
   end
 end
