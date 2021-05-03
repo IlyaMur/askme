c648750776d54668418c2c7ef09a1929c7c72c3a
Ilya Mur <ilyamur@hotmail.com>
2021-05-04 01:32:01 +0300

    fix typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :questions, except: %i[show new index]
   resources :users
-  resources :session, only: %i[new create destroy]
+  resource :session, only: %i[new create destroy]
   resources :hashtags, only: %i[show]
   root 'users#index'
 end
