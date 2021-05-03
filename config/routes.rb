0aea4c5a49d2fbc12f820518d61e0f1add7f1335
Ilya Mur <ilyamur@hotmail.com>
2021-05-04 01:21:11 +0300

    routes shortcuts, security fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   resources :questions, except: %i[show new index]
   resources :users
-  resources :sessions, only: %i[new create destroy]
+  resources :session, only: %i[new create destroy]
   resources :hashtags, only: %i[show]
   root 'users#index'
-
-  get 'sign_up' => 'users#new'
-  get 'log_out' => 'sessions#destroy'
-  get 'log_in' => 'sessions#new'
 end
